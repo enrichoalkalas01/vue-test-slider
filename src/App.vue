5c0f60b151c8628b47b124864e9a499b0616d160
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-03-10 23:03:56 +0700

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Flicking :options="{ align: 'prev', circular: true }">
+      <div class="panel">
+        <div class="image-data" style="background-image:url('https://i.pinimg.com/originals/ef/7f/b1/ef7fb1b37078b6a2aef8e40710446bfa.jpg')"></div>
+      </div>
+      <div class="panel">
+        <div class="image-data" style="background-image:url('https://wallpapercave.com/wp/wp2719096.jpg')"></div>
+      </div>
+      <div class="panel">
+        <div class="image-data" style="background-image:url('https://i.pinimg.com/736x/7d/98/84/7d98840fdff1b2e7cd508cc7f3a17403.jpg')"></div>
+      </div>
+    </Flicking>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Flicking } from "@egjs/vue-flicking"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Flicking: Flicking
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url("../node_modules/@egjs/vue-flicking/dist/flicking.css");
+  .panel {
+    width: 100%;
+    height: 500px;
+  }
+
+  .image-data {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: #dedede;
+  }
 </style>
